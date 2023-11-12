0f63d39e6f689ed572a7c891b208025a0cfdce20
”Вагин <m.vagin@k-telecom.org>
2023-11-12 18:05:54 +0500

    add comment

--- src/main/java/ru/akalavan/validParentheses/Solution.java
+++ src/main/java/ru/akalavan/validParentheses/Solution.java
@@ -1,8 +1,35 @@
 package ru.akalavan.validParentheses;
 
-import java.util.Arrays;
 import java.util.Stack;
 
+/**
+ * Given a string s containing just the characters '(', ')', '{', '}', '[' and ']', determine if the input string is valid.
+ * <p>
+ * An input string is valid if:
+ * <p>
+ * Open brackets must be closed by the same type of brackets.
+ * Open brackets must be closed in the correct order.
+ * Every close bracket has a corresponding open bracket of the same type.
+ * <p>
+ * <p>
+ * Example 1:
+ * <p>
+ * Input: s = "()"
+ * Output: true
+ * Example 2:
+ * <p>
+ * Input: s = "()[]{}"
+ * Output: true
+ * Example 3:
+ * <p>
+ * Input: s = "(]"
+ * Output: false
+ * <p>
+ * Constraints:
+ * <p>
+ * 1 <= s.length <= 104
+ * s consists of parentheses only '()[]{}'.
+ */
 public class Solution {
     public boolean isValid(String s) {
         if (s.length() < 2) {
@@ -10,7 +37,7 @@ public class Solution {
         }
 
         Stack<Character> brackets = new Stack<>();
-        for (char c: s.toCharArray()) {
+        for (char c : s.toCharArray()) {
             if (c == '(') {
                 brackets.push(')');
             } else if (c == '{') {
